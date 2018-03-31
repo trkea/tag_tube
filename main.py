bd28f9e69c945a7d049e74c4e62cf43bf5882caa
trkea <trs1141@yahoo.co.jp>
2018-03-31 20:16:34 +0900

    remove space

--- main.py
+++ main.py
@@ -14,48 +14,28 @@ def send_URL():
 @route('/tag_tube/result',method=["POST","GET"])
 
 def result():
-
     movie_tag = ""
-
     title = ""
-
     try:
-
         url =  request.forms.get('URL') 
-
         html = urllib2.urlopen(url)
-
     except Exception:
-
         title = u"エラー URLが不正です"
-
         return   template("result",title=title,movie_tag=movie_tag)
-
     if url.find("youtube") == -1:
-
         title = u"エラー YoutubeのURLを入力して下さい"
-
         return  template("result",title=title,movie_tag=movie_tag)
 
-
     youtube = youtubeValue.youtubeValue(url)
-
     movie_tag = youtube.get_movie_tag()
-
     title = youtube.get_movie_title()
 
     if title == None and movie_tag == "":
-
         title = u"この動画は存在しません"
-
     elif not title == None and movie_tag == "":
-
         movie_tag = u"なし"
-
     elif title == None and not movie_tag == "":
-
         title = u"動画タイトルの読み込みに失敗しました"
-
     return  template("result",title=title,movie_tag=movie_tag)
 
 run(host='localhost', port=8081, debug=True)
--- youtubeValue.py
+++ youtubeValue.py
@@ -4,47 +4,31 @@ import urllib2
 from bs4 import BeautifulSoup
 
 def get_HTML(url):
-    
     html = urllib2.urlopen(url)
-
     soup = BeautifulSoup(html, "html.parser")
-
     return soup
 
 class youtubeValue():
 
     def __init__(self,url):
-
         self.url = url
 
     def get_movie_tag(self):
-
         get_tag = ""
-
         soup = get_HTML(self.url)
-
         meta = soup.find_all("meta")
-
         for tag in meta:
-
             try:
                 string_ = tag.get("name")
-
                 if not string_ == None and string_ in "keywords":
                     get_tag = tag.get("content")
                     break
-
             except AttributeError:
                 pass
-
         return get_tag
 
     def get_movie_title(self):
-
         soup = get_HTML(self.url)
-
         title = soup.find("title").string
-
         get_title= title.rstrip(" - YouTube")
-
         return get_title
